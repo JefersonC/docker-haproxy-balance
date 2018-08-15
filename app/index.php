aee0ca218c1cc3eb48c96dfedb6af2b720e9649d
Jeferson Capobianco <jeferson.capobianco@gmail.com>
2018-08-15 13:47:17 -0300

    First commit

---
+++ app/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body style="text-align: center">
+    <h1>Olá, eu sou o contâiner <?= $_SERVER['HOSTNAME'] ?></h1>
+    <h3>Meu IP é <?= $_SERVER['SERVER_ADDR'] ?></h3>
+  </body>
+</html>
